6350105ace9e9f1be2404c3ad3adad90d52f965c
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-26 04:13:56 +0800

    Finished and added 1.10 Coding Exercise

---
+++ 1.10-CodingExercise/main.py
@@ -0,0 +1,10 @@
+
+character_job = "pilot"
+character_ride = "plane"
+character_souvenir = "fruits"
+character_pet = "lion"
+
+print("There was once a " + character_job + " who loved adventure.")
+print("The " + character_job + " would take her " + character_ride + " to unknown places.")
+print("She brings home a lot of " + character_souvenir)
+print("Then she goes home to her pet " + character_pet + ", Chuckles.")
