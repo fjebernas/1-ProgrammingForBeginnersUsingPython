7784702c7b62205592774bb7f1a76aff6bd9e200
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-26 15:04:25 +0800

    Finished and added 2.11 Coding exercise

---
+++ 2.11-CodingExercise/main.py
@@ -0,0 +1,21 @@
+
+def problem1():
+    return (10 > 7)
+
+def problem2():
+    return str(73911)
+
+def problem3():
+    return tuple("Thank God it's Friday!")
+
+def problem4():
+    return float(4302)
+
+def problem5():
+    return int(32399.35640)
+
+print(problem1())
+print(problem2())
+print(problem3())
+print(problem4())
+print(problem5())
