4d47f553defb20bd0b3a47be320abb3d6ed30f4f
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-26 17:55:07 +0800

    Finished and added 3.12 Coding Exercise

---
+++ 3.12-CodingExercise/main.py
@@ -0,0 +1,26 @@
+
+def problem1():
+    return print(mom_age < sister_age and my_age == 22)
+
+def problem2():
+    return print(mom_age == 61)
+
+def problem3():
+    return print(mom_age > 34 or sister_age == 22)
+
+def problem4():
+    return print(mom_age >= 54)
+
+def problem5():
+    return print(not(sister_age <= 44 and my_age == 22))
+
+my_age = 22
+mom_age = 61
+sister_age = 29
+
+problem1()
+problem2()
+problem3()
+problem4()
+problem5()
+
