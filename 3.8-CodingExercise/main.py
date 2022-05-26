551d9cbeca05a69c4afeeb89a38e13aade9c1d87
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-26 17:32:56 +0800

    Finished and added 3.8 Coding exercise

---
+++ 3.8-CodingExercise/main.py
@@ -0,0 +1,21 @@
+
+def problem1():
+    return print(217 * 6)
+
+def problem2():
+    return print(600 + 35234)
+
+def problem3():
+    return print(67 // 12)
+
+def problem4():
+    return print(56329 % 982)
+
+def problem5():
+    return print(34 ** 5)
+
+problem1()
+problem2()
+problem3()
+problem4()
+problem5()
